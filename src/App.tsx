5411e4c261e139fe37ed07f9fab4e512b484c8cd
FelipeK57 <kevinboor5725@gmail.com>
2025-08-15 13:48:50 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>
+          Hello world!
+        </h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
