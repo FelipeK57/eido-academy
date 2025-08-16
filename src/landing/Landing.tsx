09e71f1156c0f7914cc59d013bde6f9a8d22df02
FelipeK57 <kevinboor5725@gmail.com>
2025-08-16 14:11:01 -0500

    feat: add aurora animation and enhance landing page with motion effects

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, BookOpen, LibraryBig, Pickaxe, TrendingUp } from "lucide-react"
 import { Link } from "react-router"
 import { NumberTicker } from "../components/magicui/number-ticker"
+import { motion } from "motion/react"
 
 export const Landing = () => {
 
@@ -41,36 +42,63 @@ export const Landing = () => {
     ]
 
     return <main className="flex flex-col">
-        <header className="fixed w-full p-4 bg-white shadow-xs">
-            <div className="flex items-center justify-between mx-auto max-w-7xl">
+        <motion.header
+            initial={{
+                y: -200
+            }}
+            animate={{
+                y: 0
+            }}
+            transition={{
+                type: "spring",
+                stiffness: 350,
+                damping: 60,
+                duration: 0.5
+            }}
+            className="fixed w-full p-4 bg-white shadow-xs">
+            <div className="flex items-center justify-between mx-auto max-w-6xl">
                 <div className="flex gap-2 items-center">
                     <BookOpen className="stroke-blue-600" />
                     <p className="font-bold text-blue-800">Eido Academy</p>
                 </div>
                 <div className="hidden md:flex gap-10">
                     <Link to="/courses"
-                        className="hover:text-blue-600 transition-all"
+                        className="hover:text-blue-600 hover:-translate-y-0.5 transition-all"
                     >Cursos</Link>
                     <Link to="/admin"
-                        className="hover:text-blue-600 transition-all"
+                        className="hover:text-blue-600 hover:-translate-y-0.5 transition-all"
                     >Administración</Link>
                 </div>
             </div>
-        </header>
+        </motion.header>
         <section className="flex items-center justify-center min-h-svh px-4 bg-blue-50">
-            <div className="flex flex-col items-center gap-10 max-w-7xl mx-auto">
+            <motion.div
+                initial={{
+                    opacity: 0
+                }}
+                whileInView={{
+                    opacity: 1
+                }}
+                transition={{
+                    type: "spring",
+                    duration: 1
+                }}
+                viewport={{
+                    once: true
+                }}
+                className="flex flex-col items-center gap-10 max-w-6xl mx-auto">
                 <div className="flex gap-1 px-4 py-2 rounded-lg bg-blue-100">
                     🚀 <span className="font-medium text-blue-600">Plataforma educativa lider</span>
                 </div>
-                <h1 className="text-center font-bold text-4xl sm:text-5xl md:text-7xl">Aprende nuevas habilidades en <span className="text-blue-600">Eido Academy</span></h1>
+                <h1 className="text-center font-bold text-4xl sm:text-5xl md:text-7xl">Aprende nuevas habilidades en <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-600 to-blue-500">Eido Academy</span></h1>
                 <p className="text-center md:text-xl text-gray-600 max-w-lg md:max-w-2xl">Descubre miles de cursos online diseñados por expertos. Desde programación hasta diseño, encuentra el conocimiento que necesitas para impulsar tu carrera profesional.</p>
-                <button className="flex items-center font-semibold gap-2 px-4 py-2 rounded-lg bg-blue-600 text-white cursor-pointer active:scale-95 hover:bg-blue-800 transition-all">
-                    Explorar cursos <ArrowRight />
+                <button className="flex items-center gap-2 px-4 py-2 text-lg rounded-lg bg-blue-600 text-white cursor-pointer active:scale-95 hover:bg-blue-800 transition-all group">
+                    Explorar cursos <ArrowRight className="group-hover:translate-x-0.5 transition-all" />
                 </button>
-            </div>
+            </motion.div>
         </section>
         <section className="py-16 bg-white">
-            <div className="max-w-7xl mx-auto px-4">
+            <div className="max-w-6xl mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
                     <div className="space-y-2">
                         <div className="text-4xl font-bold text-blue-600"><NumberTicker className="text-4xl font-bold text-blue-600" value={50} />+</div>
@@ -92,15 +120,30 @@ export const Landing = () => {
                 <h2 className="font-bold text-3xl text-center">¿Por qué elegir Eido Academy?</h2>
                 <p className="text-center md:text-xl text-gray-600 max-w-lg md:max-w-2xl">Ofrecemos una experiencia de aprendizaje única con herramientas modernas y contenido de calidad.</p>
             </div>
-            <div className="max-w-7xl mx-auto px-4">
+            <div className="max-w-6xl mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
                     {
                         characteristics.map((characteristic, index) => (
-                            <div key={index} className="flex flex-col items-center gap-2 px-8 py-12 rounded-lg bg-white border-1 border-gray-200 transition-all hover:shadow-sm">
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                }}
+                                whileInView={{
+                                    opacity: 1,
+                                }}
+                                transition={{
+                                    type: "spring",
+                                    duration: 1,
+                                    delay: (index + 1) * 0.2,
+                                }}
+                                viewport={{
+                                    once: true
+                                }}
+                                key={index} className="flex flex-col items-center gap-2 px-8 py-12 rounded-lg bg-white border-1 border-gray-200 transition-all hover:shadow-sm">
                                 <div className="flex items-center justify-center p-4 rounded-full bg-blue-100">{characteristic.icon}</div>
                                 <span className="text-lg font-semibold">{characteristic.title}</span>
                                 <span className="text-sm text-gray-600">{characteristic.text}</span>
-                            </div>
+                            </motion.div>
                         ))
                     }
                 </div>
@@ -108,16 +151,32 @@ export const Landing = () => {
         </section>
         <section className="flex flex-col gap-10 items-center justify-center py-24 px-4 bg-white">
             <h2 className="font-bold text-3xl px-4 text-center">Lo que dicen nuestros usuarios</h2>
-            <div className="max-w-7xl mx-auto px-4">
+            <div className="max-w-6xl mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {
                         reviews.map((review, index) => (
-                            <div key={index} className="flex flex-col gap-2 px-8 py-12 rounded-lg bg-white border-1 border-gray-200 transition-all hover:shadow-sm">
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                }}
+                                whileInView={{
+                                    opacity: 1,
+                                }}
+                                transition={{
+                                    type: "spring",
+                                    duration: 1,
+                                    delay: (index + 1) * 0.2,
+                                    ease: "easeIn"
+                                }}
+                                viewport={{
+                                    once: true
+                                }}
+                                key={index} className="flex flex-col gap-2 px-8 py-12 rounded-lg bg-white border-1 border-gray-200 transition-all hover:shadow-sm">
                                 <p>⭐⭐⭐⭐⭐</p>
                                 <span className="text-lg font-semibold">{review.user}</span>
                                 <span className="text-sm text-gray-600">{review.occupation}</span>
                                 <p className="text-gray-700">"{review.review}"</p>
-                            </div>
+                            </motion.div>
                         ))
                     }
                 </div>
@@ -126,12 +185,12 @@ export const Landing = () => {
         <section className="flex flex-col gap-8 items-center justify-center py-24 px-4 bg-blue-600 text-white">
             <h2 className="font-bold text-3xl px-4 text-center">¿Listo para comenzar tu viaje de aprendizaje?</h2>
             <p className="text-center md:text-xl text-gray-200 max-w-lg md:max-w-2xl">Únete a miles de estudiantes que ya están transformando sus carreras con Eido Academy.</p>
-            <button className="flex items-center font-semibold gap-2 px-4 py-2 rounded-lg bg-white text-blue-600 cursor-pointer active:scale-95 hover:bg-gray-100 transition-all">
-                Comenzar ya <ArrowRight />
+            <button className="flex items-center font-semibold gap-2 px-4 py-2 rounded-lg bg-white text-blue-600 cursor-pointer active:scale-95 hover:bg-gray-100 transition-all group">
+                Comenzar ya <ArrowRight className="group-hover:translate-x-0.5 transition-all" />
             </button>
         </section>
         <footer className="flex flex-col py-8 px-4 gap-6 bg-blue-950 text-white">
-            <div className="flex justify-between w-full max-w-7xl mx-auto">
+            <div className="flex justify-between gap-4 w-full max-w-6xl mx-auto">
                 <div className="flex flex-col gap-4">
                     <div className="flex gap-2 items-center">
                         <BookOpen className="stroke-blue-50" />
@@ -143,7 +202,7 @@ export const Landing = () => {
                 </div>
                 <div className="flex flex-col gap-4">
                     <h3 className="font-semibold">Cursos en:</h3>
-                    <ul className="space-y-2 text-gray-400">
+                    <ul className="space-y-2 text-gray-400 text-sm">
                         <li className="hover:text-white transition-colors">
                             Desarrollo Web
                         </li>
@@ -159,7 +218,7 @@ export const Landing = () => {
                     </ul>
                 </div>
             </div>
-            <div className="w-full max-w-7xl mx-auto border-b-1 pb-1 border-b-gray-400" />
+            <div className="w-full max-w-6xl mx-auto border-b-1 pb-1 border-b-gray-400" />
             <div className="border-t border-gray-800 text-center text-gray-400">
                 <p>&copy; 2025 Eido Academy. Todos los derechos reservados.</p>
             </div>
